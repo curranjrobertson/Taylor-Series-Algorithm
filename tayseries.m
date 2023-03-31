a4040f7fd0080e8d85233bcdeca563e839be14d2
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-03-31 15:52:37 -0600

    update

--- tayseries.m
+++ tayseries.m
@@ -2,13 +2,15 @@
 % Taylor Series Algorithm
 
 clear all; clc; close all
+
 syms f(x) x
+
 N = 10; % Number of iterations
-f(x) = cos(x); % function
+f(x) = sin(x)^2; % function
 a = 5; % x location
 
-fa = f(a) % function value at a
-i = 1 % init
+fa = f(a); % function value at a
+i = 1; % init
 while i < N
     sum = fa + diff(f(x), x, i)*(x-a)/(factorial(i));
     i = i + 1;
@@ -17,6 +19,6 @@ end
 
 pretty(fa);
 
-fplot(fa, [a-5, a+5]);
+fplot(fa, [a-1, a+1]);
 hold on
-fplot(f(x),[a-5, a+5], 'r');
+fplot(f(x),[a-1, a+1], 'r');
